89ab32e3203b11e882371ca54843a19bd4bbbf74
Saman Zhiani <sampayne69@gmail.com>
2026-06-05 14:58:31 +0330

    clean code

--- library_app/urls.py
+++ library_app/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main, name='main_page'),
-    path('book_list/', views.book_list, name='book_list'),
-    path('book_detail/<int:id>/', views.book_detail, name='book_detail'),
-    path('book_form/', views.book_create_update, name='book_form'),
-    path('book_update/<int:id>/', views.book_create_update, name='book_update'),
+    path("", views.main, name="main_page"),
+    path("book_list/", views.book_list, name="book_list"),
+    path("book_detail/<int:id>/", views.book_detail, name="book_detail"),
+    path("book_form/", views.book_create_update, name="book_form"),
+    path("book_update/<int:id>/", views.book_create_update, name="book_update"),
 ]
--- library_app/views.py
+++ library_app/views.py
@@ -2,19 +2,22 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Book
 from .forms import BookForm
 
+
 # Create your views here.
 def main(request):
-    return render(request, 'library_app/main.html')
+    return render(request, "library_app/main.html")
+
 
 def book_list(request):
     books = Book.objects.all()
-    context = {'books': books}
-    return render(request, 'library_app/book_list.html', context)
+    context = {"books": books}
+    return render(request, "library_app/book_list.html", context)
+
 
 def book_detail(request, id):
     details = Book.objects.get(id=id)
-    context = {'details': details}
-    return render(request, 'library_app/book_detail.html', context)
+    context = {"details": details}
+    return render(request, "library_app/book_detail.html", context)
 
 
 def book_create_update(request, id=None):
@@ -24,18 +27,14 @@ def book_create_update(request, id=None):
     else:
         book = None
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = BookForm(request.POST, instance=book)
 
         if form.is_valid():
             form.save()
-            return redirect('book_list')
+            return redirect("book_list")
 
     else:
         form = BookForm(instance=book)
 
-    return render(
-        request,
-        'library_app/book_form.html',
-        {'form': form}
-    )
+    return render(request, "library_app/book_form.html", {"form": form})
